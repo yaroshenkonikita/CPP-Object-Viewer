ccf1b43792ae11c965f47f09482a80c3627f7c8e
Nikita Yaroshenko <yaroshenkonikita4@gmail.com>
2023-04-11 22:56:03 +0300

    fix dump

--- src/ObjectModel.cpp
+++ src/ObjectModel.cpp
@@ -3,7 +3,7 @@
 ObjectModel* ObjectModel::instance{};
 
 ObjectModel::ObjectModel() {
-    clear();
+    models.reserve(0);
 }
 
 void ObjectModel::reserve(std::size_t size) {
@@ -11,11 +11,12 @@ void ObjectModel::reserve(std::size_t size) {
 }
 
 void ObjectModel::AddNewObject() {
-    models.reserve(models.capacity() + 1);
+    models.reserve(models.size() + 1);
+    models.push_back(ObjectModel::PartObject());
 }
 
 std::size_t ObjectModel::size() {
-    return models.capacity();
+    return models.size();
 }
 
 bool ObjectModel::empty() {
@@ -23,7 +24,7 @@ bool ObjectModel::empty() {
 }
 
 void ObjectModel::clear() {
-    models.reserve(0);
+    models.clear();
 }
 
 ObjectModel::PartObject &ObjectModel::back() {
@@ -37,21 +38,17 @@ void ObjectModel::OpenObject(std::string line) {
     std::ifstream file_in(line);
     clear();
     while (std::getline(file_in, line)) {
-
         if (line.empty()) {
             continue;
         }
         if (line[0] == 'g') {
-            std::cout << "g" << std::endl;
             AddNewObject();
         } else if (line.length() < 2) {
             continue;
         } else if (line[0] == 'v' && line[1] == ' ') {
             if (empty()) {
                 AddNewObject();
-                std::cout << "g" << std::endl;
             }
-            std::cout << "v";
             int count_vertex{};
             for (unsigned i{}; i < line.length(); ++i) {
                 if (std::isdigit(line[i]) || line[i] == '-') {
@@ -61,7 +58,7 @@ void ObjectModel::OpenObject(std::string line) {
                     ++count_vertex;
                 }
             }
-            if (!count_vertex) {
+            if (count_vertex != 3) {
                 throw std::invalid_argument("Invalid file .obj");
             }
         } else if (line[0] == 'f') {
@@ -75,10 +72,8 @@ void ObjectModel::OpenObject(std::string line) {
                     std::size_t index_stoi{};
                     face.push_back(std::abs(std::stoi(line.c_str() + i, &index_stoi)));
                     i += index_stoi;
-                    std::cout << face.back() << " ";
                 }
             }
-            std::cout << std::endl;
             back().facets.push_back(face);
         }
     }
@@ -102,21 +97,6 @@ ObjectModel::PartObject &ObjectModel::operator[](const std::size_t &index) {
     return models[index];
 }
 
-int main () {
-    ObjectModel &a = *ObjectModel::GetInstance();
-    a.OpenObject("/mnt/c/Users/12355/Documents/C8_3DViewer_v1.0-0/src/objs/cube.obj");
-    std::cout << std::endl;
-    for (double vertex : a.back().vertexes) {
-        std::cout << vertex << " ";
-    }
-    std::cout << std::endl;
-//    for (auto line : a.back().facets) {
-//        std::cout << line << " ";
-//    }
-    std::cout << "models count: " << a.size() << std::endl;
-    return 0;
-}
-
 ObjectModel::PartObject::PartObject(ObjectModel::PartObject &other) {
     facets = std::move(other.facets);
     vertexes = std::move(other.vertexes);
@@ -137,4 +117,23 @@ ObjectModel::PartObject &ObjectModel::PartObject::operator=(ObjectModel::PartObj
     facets = std::move(other.facets);
     vertexes = std::move(other.vertexes);
     return *this;
+}
+
+int main () {
+    ObjectModel &a = *ObjectModel::GetInstance();
+    a.OpenObject("/mnt/c/Users/12355/Documents/C8_3DViewer_v1.0-0/src/objs/cube.obj");
+    a.OpenObject("/mnt/c/Users/12355/Documents/C8_3DViewer_v1.0-0/src/objs/cube.obj");
+    a.OpenObject("/mnt/c/Users/12355/Documents/C8_3DViewer_v1.0-0/src/objs/cube.obj");
+    for (double vertex : a.back().vertexes) {
+        std::cout << vertex << " ";
+    }
+    std::cout << std::endl;
+    for (auto line : a.back().facets) {
+        for (unsigned item : line) {
+            std::cout << item << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "models count: " << a.size() << std::endl;
+    return 0;
 }
