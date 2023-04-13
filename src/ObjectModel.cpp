af25d2590d6d49a7eeee58b32f5124d731e24e13
tokimikichika <yaroshenkonikita4@gmail.com>
2023-04-13 10:44:25 +0300

    add start position

--- src/ObjectModel.cpp
+++ src/ObjectModel.cpp
@@ -122,27 +122,28 @@ void ObjectModel::Scale(double coordinate) {
     }
 }
 
-
-ObjectModel::PartObject::PartObject(ObjectModel::PartObject &other) {
-    facets = std::move(other.facets);
-    vertexes = std::move(other.vertexes);
-}
-
-ObjectModel::PartObject::PartObject(ObjectModel::PartObject &&other) noexcept {
-    facets = std::move(other.facets);
-    vertexes = std::move(other.vertexes);
-}
-
-ObjectModel::PartObject &ObjectModel::PartObject::operator=(ObjectModel::PartObject &other) {
-    facets = std::move(other.facets);
-    vertexes = std::move(other.vertexes);
-    return *this;
-}
-
-ObjectModel::PartObject &ObjectModel::PartObject::operator=(ObjectModel::PartObject &&other) {
-    facets = std::move(other.facets);
-    vertexes = std::move(other.vertexes);
-    return *this;
+void ObjectModel::RelocateOnStartPosition() {
+    if (models.empty()) {
+        throw std::logic_error("Can't relocate. Haven't exist model");
+    }
+    double positionAxis[6] = {DBL_MAX, DBL_MAX, DBL_MAX, -DBL_MAX, -DBL_MAX, -DBL_MAX};
+    for (auto& model : models) {
+        for (std::size_t index = 0; index < model.vertexes.size(); index += 3) {
+            positionAxis[xAxis] = std::min(positionAxis[xAxis], model.vertexes[index + xAxis]);
+            positionAxis[yAxis] = std::min(positionAxis[yAxis], model.vertexes[index + yAxis]);
+            positionAxis[zAxis] = std::min(positionAxis[zAxis], model.vertexes[index + zAxis]);
+            positionAxis[xAxis + 3] = std::max(positionAxis[xAxis + 3], model.vertexes[index + xAxis]);
+            positionAxis[yAxis + 3] = std::max(positionAxis[yAxis + 3], model.vertexes[index + yAxis]);
+            positionAxis[zAxis + 3] = std::max(positionAxis[zAxis + 3], model.vertexes[index + zAxis]);
+        }
+    }
+    double deltaAxis[3] = {positionAxis[xAxis + 3] - positionAxis[xAxis],
+                           positionAxis[yAxis + 3] - positionAxis[yAxis],
+                           positionAxis[zAxis + 3] - positionAxis[zAxis]};
+    Move(-(positionAxis[xAxis] + deltaAxis[xAxis] / 2), xAxis);
+    Move(-(positionAxis[yAxis] + deltaAxis[yAxis] / 2), yAxis);
+    Move(-(positionAxis[zAxis] + deltaAxis[zAxis] / 2), zAxis);
+    Scale(1/std::max(std::max(deltaAxis[xAxis], deltaAxis[yAxis]), deltaAxis[zAxis]));
 }
 
 #include <iostream>
